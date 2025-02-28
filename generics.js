c870899f1c5d02a1507b4b5feb91547bd70eea15
Abhijit Patra <apatra@juniper.net>
2025-02-28 20:15:47 +0530

    Generics merged

---
+++ generics.js
@@ -0,0 +1,55 @@
+//<T>
+// function printName<T>(arg : T) : T {
+//     return arg;
+// }
+// const printName1 = <T>(arg: T): T=> {
+//     return arg;
+// }
+// console.log(printName1<string>("abh"));
+// console.log(printName<string>("abhi"));
+// interface Person<T> {
+//     EmployeeId : T
+// }
+// let p : Person<string| number> =  {
+//     EmployeeId: "1000210"
+// }
+// console.log(p.EmployeeId);
+// type Person<T> =  {
+//     EmployeeId : number,
+//     name: string,
+//     addrerss: T
+// }
+// type address = {
+//     pincode: number,
+//     location: string
+// }
+// let p1: Person<address> = {
+//     EmployeeId: 102020,
+//     name : "abhi",
+//     addrerss: {
+//         pincode: 1020222,
+//         location: "Kolkata"
+//     }
+// }
+// console.log(p1.addrerss.location)
+// class PrintNames<T> {
+//     private names: T[] = [];
+//     // constructor(name: T) {
+//     //     // this.names.push(name);
+//     // }
+//     setName(name: T) {
+//         this.names.push(name)
+//     }
+//    getNames(): T[] {
+//      return this.names;
+//    }
+// }
+// let name_1 = new PrintNames<string>();
+// name_1.setName("a");
+// name_1.setName("b");
+// console.log(name_1.getNames());
+function PrintName(arg) {
+    return arg.age;
+}
+// console.log(PrintName(123456))
+console.log(PrintName({ age: 20, name: "aaa" }));
